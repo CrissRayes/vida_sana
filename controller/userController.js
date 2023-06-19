2e8d1b1cdfabacae8b01293f6da4453be1d8f05e
Cristian Ramírez <cristian.ramirez.cl@gmail.com>
2023-06-19 14:32:45 -0400

    login pipeline creado

--- controller/userController.js
+++ controller/userController.js
@@ -1,10 +1,31 @@
-const { queryEvents } = require('../models/queries');
+const jwt = require('jsonwebtoken');
+const { queryEvents, validateCredentials } = require('../models/queries');
 
 const getAllEvents = async (req, res, next) => {
   const events = await queryEvents();
   res.json(events);
 };
 
+const loginUser = async (req, res, next) => {
+  try {
+    // obtener email y pass del body de la request
+    const { email, password } = req.body;
+    // verificar si las credenciales son correctas
+    await validateCredentials(email, password);
+    // generar un token
+    const token = jwt.sign({ email }, process.env.JWT_SECRET, {
+      expiresIn: '1d',
+    });
+    // enviar respuesta con el token
+    res.status(200).json(token);
+  } catch (error) {
+    error.status = 'fail';
+    error.statusCode = 500;
+    return next(error);
+  }
+};
+
 module.exports = {
   getAllEvents,
+  loginUser,
 };
--- models/queries.js
+++ models/queries.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const pool = require('./db');
 
 const queryEvents = async () => {
@@ -6,6 +7,34 @@ const queryEvents = async () => {
   return eventos;
 };
 
+const validateCredentials = async (email, password) => {
+  // email y contraseña ingresados?
+  if (!email) {
+    throw new Error('Email no ingresado');
+  }
+  if (!password) {
+    throw new Error('Contraseña no ingresada');
+  }
+
+  // Obtener data
+  const sqlQuery = 'SELECT * FROM usuarios WHERE email = $1';
+  const values = [email];
+  const { rows } = await pool.query(sqlQuery, values);
+  const user = rows[0];
+
+  // existe el usuario?
+  if (!rows.length === 0) {
+    throw new Error('El usuario no existe');
+  }
+
+  // coinciden las contraseñas?
+  const isValid = bcrypt.compareSync(password, user.password);
+  if (!isValid) {
+    throw new Error('Contraseña incorrecta');
+  }
+};
+
 module.exports = {
   queryEvents,
+  validateCredentials,
 };
